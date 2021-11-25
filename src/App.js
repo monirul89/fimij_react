db8969d0b292e2c63352ab0179c3c9e19ba1d368
monirul89 <monirul89@gmail.com>
2021-11-25 15:01:26 +0600

    update login validation

--- src/App.js
+++ src/App.js
@@ -4,38 +4,48 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component{
 
   state ={
-    user_name:'',
-    user_number:'',
     user_email:'',
-    user_address:''
+    user_password:'',
+    user_emailError:'',
+    user_passwordError:''
   }
-  changeData1=(e)=>{
+  validate(){
+    if(!this.state.user_email>0 && !this.state.user_password>0){
+      this.setState({
+        user_emailError:'Email and password required'
+      })
+    }else if(!this.state.user_email>0){
+      this.setState({
+        user_emailError:'Email required'
+      })
+    }else if(!this.state.user_password>0){
+      this.setState({
+        user_passwordError:'Password required'
+      })
+    }else{
+      return true;
+    }
+  }
+  formSubmit = (e)=>{
     e.preventDefault();
-    let name = e.target.name;
-    let value = e.target.value;
-    this.setState({
-      [name]:value
-    });
-    console.log(value);
+    if(this.validate()){
+        alert("Form submit done");
+    }
   }
+  
   render(){
 
     return (
       <div className="App">
           
-          <form onSubmit={this.changeData1}>
+          <form onSubmit={this.formSubmit}>
               <div className="form-group" style={{width:'320px', margin:'0 auto', textAlign: 'left'}}>
-                {/* <h4>HELLO {this.state.username}</h4> */}
-                <div className="show-data">
-                    <ul style={{listStyleType:'none'}}>
-                      <li>{this.state.user_name}</li>
-                      <li>{this.state.user_number}</li>
-                      <li>{this.state.user_email}</li>
-                      <li>{this.state.user_address}</li>
-                    </ul>
-                </div>
-                <label>User Name:</label><br></br>
-                <input className="form-control" onChange={this.changeData} type="email" name="user_email" /><br></br>
+                <label>User email</label><br></br>
+                <input className="form-control" onChange={(e)=>{this.setState({user_email:e.target.value})}} type="email" name="user_email" /><br/>
+                <p style={{color:'red'}}>{this.state.user_emailError}</p><br/>
+                <label>Password</label><br></br>
+                <input className="form-control" onChange={(e)=>{this.setState({user_password:e.target.value})}} type="password" name="user_password" /><br/>
+                <p style={{color:'red'}}>{this.state.user_passwordError}</p><br/>
                 <button className="btn btn-success" type="submit">Submit</button>
               </div>
           </form>
