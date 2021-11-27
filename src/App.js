254d57b0ec8d7d7e39e95daf6dc4255bbde339d0
monirul89 <monirul89@gmail.com>
2021-11-27 14:14:33 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './component/home/home'
 function App() {
   return (
-    <div className="App">
-        <Button variant="primary">Primary</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="success">Success</Button>
-        <Button variant="warning">Warning</Button>
-        <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>
-        <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>
-        <Button variant="link">Link</Button>
+    <div className="body_parant">
+      <Home />
     </div>
   );
 }
---
+++ src/component/home/home.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import Header  from "../header";
+class Home extends Component{
+    render(){
+        return <div className="home-page">
+            <Header />
+        </div>
+    }
+}
+
+export default Home;
